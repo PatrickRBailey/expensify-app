284b2199036178ded099c54504af6df4d4eb9456
Patrick Bailey <patrick@ieqtechnology.com>
2018-05-10 10:01:51 -0700

    destructuring arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// Object Destructuring
+
 // const person = {
 //   name: 'Patrick',
 //   age: 35,
@@ -17,14 +19,25 @@
 //   console.log(`It's ${temp} in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin',
-  },
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin',
+//   },
+// };
 
-const { name: publisherName = 'Self-published' } = book.publisher;
+// const { name: publisherName = 'Self-published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array Destructuring
+
+// const address = ['1299 S Apple St', 'Springfield', 'Oregon', '98234'];
+// const [, city, state] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+// const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+// const [type,, medium] = item;
+
+// console.log(`A medium ${type} costs ${medium}`);
